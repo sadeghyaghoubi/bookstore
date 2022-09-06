013489ffd8c5d3cef895008268780c40246ebf71
s.yaghoubi <sadegh.yaghoubi1995@gmail.com>
2022-09-06 15:27:21 +0430

    add test for home page(HomePageView)

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTest(TestCase):
+##barresi khode URL : agar to url aslie proje esmesh taghir kone in khata mide
+    def test_home_page_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+##agar esme url taghir kone inja khata mide
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_Title(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains (response, 'Home Page')
+
+    def Home_Page_Template_Used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
