519b090c8c054ca488c0f8e11580a085d3caaaf4
s.yaghoubi <sadegh.yaghoubi1995@gmail.com>
2022-09-12 17:42:18 +0430

    add book(list detail create update delete)views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book
+from django.urls import reverse_lazy
 
-# Create your views here.
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'books'   #be dard halghe for too safhe listemoon mikhore
+    template_name = 'book/book_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    template_name = 'book/book_create.html'
+    fields = ['title', 'description', 'author', 'price', ]
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'description', 'author', 'price', ]
+    template_name = 'book/book_update.html'
+
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'book/book_delete.html'
+    success_url = reverse_lazy('book_list')
+
+
+
