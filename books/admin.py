6a27176640a19cb72c95baae0e2c47746ddd8316
s.yaghoubi <sadegh.yaghoubi1995@gmail.com>
2022-09-12 16:43:30 +0430

    add Book Models and Config show that in admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# admin.site.register(Post)
+@admin.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'author', 'price', 'date_time_create', 'date_time_modify',)
+    ordering = ('date_time_create',)
+
--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Book (models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2, )
+    date_time_create = models.DateTimeField(auto_now_add=True)
+    date_time_modify = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[self.id])
