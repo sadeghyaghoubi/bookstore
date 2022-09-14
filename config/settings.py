a648b38cac22bae5e258461596c2bc6ecf6bf501
s.yaghoubi <sadegh.yaghoubi1995@gmail.com>
2022-09-14 13:43:07 +0430

    piade sazi backendemoon rooye front end

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -141,5 +141,6 @@ EMAIL_BACKEND= 'django.core.mail.backends.console.EmailBackend'
 
 #static file config
 STATIC_URL= '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))],
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
 
