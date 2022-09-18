8eed2635bf08c52870ce1623a5435db6d1748d55
s.yaghoubi <sadegh.yaghoubi1995@gmail.com>
2022-09-18 17:28:26 +0430

    add new table (Comment) and show in admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Book
-
+from .models import Book, Comment
 # admin.site.register(Post)
-@admin.register(Book)
 
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'author', 'price', 'date_time_create', 'date_time_modify',)
+    list_display = ('title', 'author', 'date_time_create', 'date_time_modify',)
+    ordering = ('date_time_create',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'date_time_create', )
     ordering = ('date_time_create',)
 
