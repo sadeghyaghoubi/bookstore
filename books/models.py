ba51b1ca1a3dae9b234a7aaf9ec7755a48509b45
s.yaghoubi <sadegh.yaghoubi1995@gmail.com>
2022-09-18 17:55:01 +0430

    add related cooment in models.py for comment book

--- books/models.py
+++ books/models.py
@@ -21,7 +21,7 @@ class Book (models.Model):
 
 class Comment (models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, )
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments', )
     text = models.TextField()
     date_time_create = models.DateTimeField(auto_now_add=True)
 
