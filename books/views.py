c432919d9d41b08ddef0f81b084257f3eb720b87
s.yaghoubi <sadegh.yaghoubi1995@gmail.com>
2022-09-18 19:14:22 +0430

    form nazarat dakhele site kar kone

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Book
 from django.urls import reverse_lazy
-
+from .forms import CommentForm
 class BookListView(generic.ListView):
     model = Book
     context_object_name = 'books'   #be dard halghe for too safhe listemoon mikhore
@@ -17,8 +17,23 @@ class BookListView(generic.ListView):
 def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book_comments = book.comments.all()
-    return render(request, 'book/book_detail.html', {'book': book, 'comments': book_comments})
 
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'book/book_detail.html', {
+        'book': book,
+        'comments': book_comments,
+        'comment_form': comment_form,
+    })
 
 
 class BookCreateView(generic.CreateView):
